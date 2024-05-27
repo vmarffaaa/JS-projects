57664213d8e80563c74a5223be124e8213c87084
Marta <veeenling@gmail.com>
2024-05-28 01:16:34 +0300

    3

--- HomeWork11/task3/main.js
+++ HomeWork11/task3/main.js
@@ -3,3 +3,17 @@
 //     вивести їх id + name списком та додати посилання з href = user-details.html?id=XXX (замість ХХХ - айді юзера)
 // при кліку на посилання перехід на відповідну сторінку, на якій буде вся інформація про користувача (всі 15 полів)
 // отримана через додатковий запит (https://jsonplaceholder.typicode.com/users/XXX   де ХХХ - айді користувача)
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => {
+        const userList = document.getElementById('usersList');
+        users.forEach(user => {
+            let counter = 0;
+            const listItem = document.createElement('li');
+            const link = document.createElement('a');
+            link.href = `https://jsonplaceholder.typicode.com/users/${parseInt(user.id)}`;
+            link.textContent = `${user.id} - ${user.name}`;
+            listItem.appendChild(link);
+            userList.appendChild(listItem);
+        });
+    })
